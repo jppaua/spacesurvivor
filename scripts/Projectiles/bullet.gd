2da427ee18616f6dbd42b8b6b03ba5476a15d58b
AshKretschmar <159160301+AshKretschmar@users.noreply.github.com>
2024-03-27 18:47:03 -0400

    Add files via upload

---
+++ scripts/Projectiles/bullet.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+const SPEED = 2000.0
+#@onready var timer = $Timer
+
+func _ready():
+	#timer.start()
+	pass
+
+func _physics_process(delta):
+	var direction = Vector2(cos(rotation), sin(rotation))
+	velocity = direction * SPEED
+	move_and_slide()
+
+func _on_timer_timeout():
+	queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("solid_tile"):
+		queue_free()
+		#print("hit wall")
+
+
+
+
+
+
+
+
