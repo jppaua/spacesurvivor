7bbeeb4eedd4c9cbaa71b56e675578f0955300d0
Redgamer32 <red.mario06@gmail.com>
2024-09-15 16:48:59 -0400

    Junk Pruning

--- scripts/grenade.gd
+++ scripts/grenade.gd
@@ -3,11 +3,6 @@ extends CharacterBody2D
 var speed = 1300.0
 var damage = 0
 var enemiesInRange = []
-<<<<<<< HEAD
-=======
-var knockback = Vector2(600, -600)
-var has_exploded = false
->>>>>>> 9477db447deb1bcd48b6f4670c8468cd8c03d500
 var grenade_explosion = preload("res://scenes/prefabs/grenade_explosion.tscn")
 
 func _physics_process(delta):
@@ -20,25 +15,11 @@ func _on_timer_timeout():
 	
 	
 func _on_area_2d_body_entered(body):
-<<<<<<< HEAD
-=======
-	if has_exploded:
-		return
-	has_exploded = true
->>>>>>> 9477db447deb1bcd48b6f4670c8468cd8c03d500
 	get_node("Sprite2D").visible = false
 	speed = 0
 	if body.is_in_group("solid_tile") or body.is_in_group("enemy"):
 		for enemy in enemiesInRange:
-<<<<<<< HEAD
 			enemy.take_damage(damage)
-=======
-			if enemy.has_method("take_damage"):
-				enemy.take_damage(damage)
-			if enemy.has_method("take_knockback"):
-				enemy.take_knockback(self, knockback)
-		
->>>>>>> 9477db447deb1bcd48b6f4670c8468cd8c03d500
 		var new_explosion_particles = grenade_explosion.instantiate()
 		add_child(new_explosion_particles)
 		var particles = new_explosion_particles.get_node("explosion")
