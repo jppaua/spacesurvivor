66907a93d6a8f539b7a36005deb555a18f7dc0d8
AndresGiven <andresfelipehung@gmail.com>
2024-09-18 11:28:49 -0400

    added a tracking launcher, other weapon in progress

---
+++ scenes/prefabs/orbital_laser_raycast.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var damage = 0
+var printed = false
+@onready var ray_cast = $RayCast2D
+@onready var line = $Line2D
+
+
+func _ready():
+	global_position = get_global_mouse_position() 
+
+func _draw():
+	if ray_cast.is_colliding():
+		draw_line(Vector2.ZERO, ray_cast.get_collision_point() - global_position, Color.RED, 2)
+		var collision_point = ray_cast.get_collision_point()
+		if !printed:
+			print(collision_point)
+			printed = true
+	else:
+		draw_line(Vector2.ZERO, ray_cast.target_position, Color.GREEN, 2)
+
+func _process(delta):
+	queue_redraw()
--- scenes/prefabs/tracking_rpg.gd
+++ scenes/prefabs/tracking_rpg.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
 
-var speed = 1300.0
+var speed = 900.0
 var damage = 0
-var enemiesInRange = []
-var knockback = Vector2(600, -600)
+var turn_speed = 5.0
+var enemies_in_range = []
+var knockback = Vector2(500, -500)
 var has_exploded = false
-var grenade_explosion = preload("res://scenes/prefabs/grenade_explosion.tscn")
+var has_tracked = false
+var grenade_explosion = preload("res://scenes/prefabs/seeking_grenade_explosion.tscn")
 
 func _physics_process(delta):
+	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		has_tracked = true
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and !has_tracked:
+		var mouse_position = get_global_mouse_position()
+		var direction_to_mouse = (mouse_position - position).normalized()
+		var target_rotation = direction_to_mouse.angle()
+		rotation = lerp_angle(rotation, target_rotation, turn_speed * delta)
 	var direction = Vector2(cos(rotation), sin(rotation))
 	velocity = direction * speed
 	move_and_slide()
 
 func _on_timer_timeout():
+	print("deleted")
 	queue_free()
 	
 	
@@ -24,7 +34,7 @@ func _on_area_2d_body_entered(body):
 	get_node("trail").emitting = false
 	speed = 0
 	if body.is_in_group("solid_tile") or body.is_in_group("enemy"):
-		for enemy in enemiesInRange:
+		for enemy in enemies_in_range:
 			if enemy.has_method("take_damage"):
 				enemy.take_damage(damage)
 			if enemy.has_method("take_knockback"):
@@ -41,8 +51,8 @@ func _on_area_2d_body_entered(body):
 
 func _on_explosion_hit_box_body_entered(body):
 	if body.is_in_group("enemy"):
-		enemiesInRange.append(body)
+		enemies_in_range.append(body)
 
 func _on_explosion_hit_box_body_exited(body):
 	if body.is_in_group("enemy"):
-		enemiesInRange.erase(body)
+		enemies_in_range.erase(body)
