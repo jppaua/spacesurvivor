d6f7f3d26618b49b91edb40bb4ea2d8f3c3995ff
John Paua <johnpaua17@gmail.com>
2024-09-19 04:25:44 -0400

    flying enemy

---
+++ scripts/flying_geode.gd
@@ -0,0 +1,151 @@
+extends CharacterBody2D
+
+
+const SPEED = 260.0
+const JUMP_VELOCITY = -700.0
+const MIN_DISTANCE = 300
+const MAX_DISTANCE = 450
+var air_speed_increment = 25
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var max_health = 999
+var knockbackable = false
+var health = max_health
+var previous_x_position
+var distance
+var status = "CHASING"
+var player
+var HitParticles = preload("res://scenes/prefabs/hit_particle.tscn")
+var flying_mob = true
+var flying_height = 400
+var altitude_speed = 20
+
+@onready var attack_timer = $rock_elemental_parent/Timer
+@onready var hp = $EnemyInfo/HP
+@onready var status_label = $EnemyInfo/status
+@onready var enemy_position = $EnemyInfo/position
+@onready var rock_elemental_parent = $rock_elemental_parent
+@onready var damage_numbers_origin = $DamageNumbersOrigin
+@onready var knockback_timer = $rock_elemental_parent/knockbackTimer
+
+
+func _ready():
+	player = Global.player_node
+	hp.max_value = max_health
+	hp.value = max_health
+
+func _physics_process(delta):
+	if player.global_position.x > global_position.x:
+		rock_elemental_parent.scale.x = 1
+	else:
+		rock_elemental_parent.scale.x = -1
+	
+	distance = global_position.distance_to(player.global_position)
+	if distance >= MAX_DISTANCE:
+		status = "CHASING"
+	elif distance < MIN_DISTANCE: 
+		status = "RETREATING"
+	else: 
+		status = "ATTACKING"
+	
+	var direction = -1
+	if player.global_position.x > global_position.x:
+		direction = 1
+	
+	if status == "CHASING":
+		velocity.x = direction * SPEED
+		if previous_x_position == global_position.x and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+	
+	if status == "RETREATING":
+		velocity.x = (direction * SPEED * 0.7 * -1)
+		if previous_x_position == global_position.x and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+	
+	if status == "ATTACKING" and attack_timer.time_left == 0:
+		velocity.x = 0;
+		attack()
+		attack_timer.wait_time = 2
+		attack_timer.start()
+	
+	if flying_mob:
+		if global_position.y < flying_height:
+			velocity.y += altitude_speed * delta #move downwards
+		elif global_position.y > flying_height:
+			velocity.y -= altitude_speed * delta #move upwards
+		else:
+			velocity.y = 0  # Stop vertical movement if at flying height
+	
+	if not is_on_floor() && !flying_mob:
+		velocity.y += gravity * delta
+		if direction * velocity.x < 0:
+			velocity.x += direction * air_speed_increment
+		else:
+			if abs(velocity.x) < SPEED:
+				velocity.x += direction * air_speed_increment
+	#decreases speed rapidly when not holding direction
+	else:
+		velocity.x = move_toward(velocity.x, 0, 100)
+	
+	enemy_position.text = str(global_position)
+	status_label.text = status
+	previous_x_position = global_position.x
+	move_and_slide()
+
+func attack():
+	var projectile = load("res://scenes/prefabs/rock.tscn")
+	var projectile_instance = projectile.instantiate()
+	var scale = Global.player_node.get_node("PlayerParent").scale
+	projectile_instance.global_position = global_position
+	projectile_instance.scale = scale
+	get_tree().current_scene.add_child(projectile_instance)
+
+func take_knockback(projectile, knockback):
+	if(knockbackable):
+		velocity.y = knockback.y
+		if projectile.position.x > position.x:
+			velocity.x = knockback.x * -1
+		knockback_timer.wait_time = 0.3
+		knockback_timer.start()
+		knockbackable = false
+		await knockback_timer.timeout
+		knockbackable = true
+
+func take_damage(damage):
+	health -= damage
+	hp.value = health
+	DamageNumbers.display_number(damage, damage_numbers_origin.global_position)
+	
+	
+	if health <= 0:
+		deathParticle()
+		player.num_killed += 1
+		get_node("rock_elemental_parent").visible = false
+		get_node("EnemyInfo").visible = false
+		self.collision_layer &= ~4
+		await get_tree().create_timer(0.5).timeout
+		queue_free()
+
+
+func deathParticle():
+	get_node("deathParticle").emitting = true
+
+
+#For later use probably
+func summonParticle():
+	var new_hit_particles = HitParticles.instantiate()
+	add_child(new_hit_particles)
+	var particles = new_hit_particles.get_node("hitParticles")
+	particles.emitting = true
+	await get_tree().create_timer(particles.lifetime).timeout
+	new_hit_particles.queue_free()
+
+
+
+
+
+
+
+
+
+
+
