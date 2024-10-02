16159c75426d97b6c25b474addb44fd1bdede1a3
AshK <159160301+AshKretschmar@users.noreply.github.com>
2024-10-02 18:21:25 -0400

    added all extra assets, started creating lvl1, wave system set up in level1

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -8,13 +8,13 @@ extends Node
 
 var current_wave: int
 var wave_spawn_ended: bool
-var enemies: Array = []
+var enemies: Array = [] #will store my enemies, I count them here to know when to transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_wave = 0
-	Global.current_wave = current_wave
-	enemies.clear()
+	#Global.current_wave = current_wave
+	enemies.clear() #clear the enemies before next wave
 	position_to_next_wave()
 
 
@@ -28,7 +28,7 @@ func _process(_delta):
 
 func transition_to_next_wave():
 	if wave_spawn_ended:
-		wave_spawn_ended = false
+		wave_spawn_ended = false 
 	await get_tree().create_timer(2.0).timeout
 	position_to_next_wave()
 
@@ -36,28 +36,29 @@ func transition_to_next_wave():
 
 func position_to_next_wave():
 	if enemies.size() == 0:  # Check if there are no enemies left
-		if current_wave != 0:
-			Global.moving_to_next_wave = true
+		#if current_wave != 0:
+			#Global.moving_to_next_wave = true
 		current_wave += 1
-		Global.current_wave = current_wave
+		#Global.current_wave = current_wave
 		await get_tree().create_timer(0.5).timeout
 		prepare_spawn("rockElemental", 2, 2)
 		print(current_wave)
 
 func prepare_spawn(type, modifier, mob_spawns):
 	var mob_amount = int(2 + (current_wave * modifier * 1.2))
-	var mob_wait_time: int = 2
+	var mob_wait_time: int = 2 #2 second spawn rate
 	print("mob amount: ", mob_amount)
 	if mob_amount >0:
 		var mob_spawn_rounds = floor(mob_amount/mob_spawns)
 		wave_spawn_ended = false
 		spawn_type(type, mob_spawn_rounds, mob_wait_time)
 
+#handles the actual spawning of the enemies based on the type and number of rounds
 func spawn_type(type, mob_spawn_rounds, mob_wait_time):
 	if type == "rockElemental":
 		var rock_elemental_spawn1 = $Node2enemiesD/rock_elemental_spawn1
 		var rock_elemental_spawn2 = $Node2enemiesD/rock_elemental_spawn2
-		if mob_spawn_rounds >= 1:
+		if mob_spawn_rounds >= 1: #set spawn positions
 			for i in range(int(mob_spawn_rounds)):
 				var rockElemental1 = rock_elemental_scene.instantiate()
 				rockElemental1.global_position = rock_elemental_spawn1.global_position
@@ -65,7 +66,7 @@ func spawn_type(type, mob_spawn_rounds, mob_wait_time):
 				rockElemental2.global_position = rock_elemental_spawn2.global_position
 				add_child(rockElemental1)
 				add_child(rockElemental2)
-				
+				#connect the died signal and appends to the array (when they die, they will be removed)
 				rockElemental1.connect("died", Callable(self, "_on_enemy_died"))
 				rockElemental2.connect("died", Callable(self, "_on_enemy_died"))
 				enemies.append(rockElemental1)
