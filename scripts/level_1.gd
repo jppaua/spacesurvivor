cdaf1b970edd1e5457f542d60dcbda10a5fba200
AshK <159160301+AshKretschmar@users.noreply.github.com>
2024-10-09 16:22:34 -0400

    added some basic menus and displayed waves/enemies in level1

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@onready var pause_menu = $PauseMenu/Control
+@onready var wave_info = $ScoreTracking/VBoxContainer/Wave
+@onready var enemiesremaining = $ScoreTracking/VBoxContainer/EnemiesRemaining
+var paused = false
+
 #####-WAVE BASED SYSTEM STUFF-#######
 #Handling the wave based system in the level script
 
@@ -12,19 +17,23 @@ var enemies: Array = [] #will store my enemies, I count them here to know when t
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	reset_pause_state()
 	current_wave = 0
 	#Global.current_wave = current_wave
 	enemies.clear() #clear the enemies before next wave
 	position_to_next_wave()
+	update_wave_info()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		PauseMenu()
 	  # Transition to the next wave when all enemies are defeated
 	#print (enemies)
 	if wave_spawn_ended and enemies.size() == 0:
 		print("All enemies cleared. Transitioning to next wave...")
-		transition_to_next_wave()		
+		transition_to_next_wave()
 
 func transition_to_next_wave():
 	if wave_spawn_ended:
@@ -43,6 +52,7 @@ func position_to_next_wave():
 		await get_tree().create_timer(0.5).timeout
 		prepare_spawn("rockElemental", 2, 2)
 		print(current_wave)
+		update_wave_info()
 
 func prepare_spawn(type, modifier, mob_spawns):
 	var mob_amount = int(2 + (current_wave * modifier * 1.2))
@@ -71,12 +81,32 @@ func spawn_type(type, mob_spawn_rounds, mob_wait_time):
 				rockElemental2.connect("died", Callable(self, "_on_enemy_died"))
 				enemies.append(rockElemental1)
 				enemies.append(rockElemental2)
+				update_wave_info()
 				await get_tree().create_timer(mob_wait_time).timeout
 	wave_spawn_ended = true
 
 func _on_enemy_died(enemy):
 	# Remove the enemy from the enemies array
 	if enemies.has(enemy):
-		enemies.erase(enemy)  # Remove the enemy reference from the array
+		enemies.erase(enemy) 
 		print("Enemy removed, remaining enemies:", enemies.size())
 	enemy.queue_free()
+	update_wave_info()
+
+func PauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused
+
+func reset_pause_state():
+	paused = false  
+	Engine.time_scale = 1 
+
+
+func update_wave_info():
+	wave_info.text = "Wave: %d" % current_wave
+	enemiesremaining.text = "Enemies Remaining: %d" % enemies.size()
---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/world/level_1.tscn")
+
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/world/options.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
