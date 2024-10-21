1a93851a3989012c3429e0f1a5f2a1abfbd29d38
Redgamer32 <red.mario06@gmail.com>
2024-10-21 15:29:45 -0400

    Navigation mesh and Rock elemental integration

--- scripts/rock_elemental.gd
+++ scripts/rock_elemental.gd
@@ -11,15 +11,12 @@ var max_health = 10
 var knockbackable = true
 var can_move = true
 var health = max_health
-var previous_x_position
 var distance
 var status = "CHASING"
 var player
 var HitParticles = preload("res://scenes/prefabs/hit_particle.tscn")
 signal died
 var is_dead: bool = false
-
-
 
 @onready var attack_timer = $rock_elemental_parent/Timer
 @onready var hp = $EnemyInfo/HP
@@ -30,61 +27,48 @@ var is_dead: bool = false
 @onready var knockback_timer = $rock_elemental_parent/knockbackTimer
 @onready var _stun_timer = $rock_elemental_parent/stunTimer
 
+@onready var navAgent = $NavigationAgent2D as NavigationAgent2D
 
 func _ready():
 	player = Global.player_node
 	hp.max_value = max_health
 	hp.value = max_health
+	make_path()
 
 func _physics_process(delta):
+	navAgent.get_next_path_position()
+	
 	if player.global_position.x > global_position.x:
 		rock_elemental_parent.scale.x = 1
 	else:
 		rock_elemental_parent.scale.x = -1
 	
-	distance = global_position.distance_to(player.global_position)
-	if distance >= MAX_DISTANCE:
-		status = "CHASING"
-	elif distance < MIN_DISTANCE: 
-		status = "RETREATING"
-	else: 
-		status = "ATTACKING"
+	var direction = to_local(navAgent.get_next_path_position()).normalized()
 	
-	var direction = -1
-	if player.global_position.x > global_position.x:
-		direction = 1
+	if can_move:
+		velocity.x = round(direction.x) * SPEED
 	
-	if status == "CHASING" and can_move:
-		velocity.x = direction * SPEED
-		if previous_x_position == global_position.x and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-	
-	if status == "RETREATING" and can_move:
-		velocity.x = (direction * SPEED * 0.7 * -1)
-		if previous_x_position == global_position.x and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-	
-	if status == "ATTACKING" and attack_timer.time_left == 0:
-		velocity.x = 0;
+	if status == "ATTACKING" and attack_timer.time_left == 0 or status == "RETREATING" and attack_timer.time_left == 0:
 		attack()
 		attack_timer.wait_time = 2
 		attack_timer.start()
 	
-	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		if direction * velocity.x < 0:
-			velocity.x += direction * air_speed_increment
+		if direction.x * velocity.x < 0:
+			velocity.x += direction.x * air_speed_increment
 		else:
 			if abs(velocity.x) < SPEED:
-				velocity.x += direction * air_speed_increment
+				velocity.x += direction.x * air_speed_increment
 	#decreases speed rapidly when not holding direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, 100)
 	
+	if(-0.5 > direction.y and is_on_floor() and abs(direction.x) > 0.01 ):
+		jump()
+	
 	enemy_position.text = str(global_position)
 	status_label.text = status
-	previous_x_position = global_position.x
 	move_and_slide()
 
 func attack():
@@ -128,10 +112,8 @@ func take_damage(damage):
 		SignalBus.give_exp.emit()
 		queue_free()
 
-
 func deathParticle():
 	get_node("deathParticle").emitting = true
-
 
 #For later use probably
 func summonParticle():
@@ -141,3 +123,27 @@ func summonParticle():
 	particles.emitting = true
 	await get_tree().create_timer(particles.lifetime).timeout
 	new_hit_particles.queue_free()
+
+func make_path():
+	distance = global_position.distance_to(player.global_position)
+	if distance >= MAX_DISTANCE:
+		status = "CHASING"
+		navAgent.target_position = player.global_position
+	elif distance < MIN_DISTANCE: 
+		status = "RETREATING"
+		if player.global_position.x > global_position.x:
+			navAgent.target_position = global_position + Vector2(50,0) * -1
+		else:
+			navAgent.target_position = global_position + Vector2(50,0)
+	else: 
+		status = "ATTACKING"
+
+func jump(jump_size = JUMP_VELOCITY):
+	velocity.y = jump_size
+
+#Use later to allow enemies to make large jumps
+func _on_navigation_agent_2d_link_reached(details):
+	pass
+
+func nav_timer_timeout():
+	make_path()
